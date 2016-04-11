4cfe27900f3970aa487473dca4ece4929e596627
WojciechKwasnyy <wojtekkwa@o2.pl>
2016-04-11 15:17:37 +0200

    polaczenie dziala, odbieranie do zrobienia na alpikacji dziala z serwera
    Dodawanie przyciskow funkcyjnych ("hang up" itd przy polaczeniu)

--- app/src/main/java/com/example/wojtek_asus/app/ContactsList.java
+++ app/src/main/java/com/example/wojtek_asus/app/ContactsList.java
@@ -1,41 +1,52 @@
 package com.example.wojtek_asus.app;
 
-import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.widget.SimpleAdapter;
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-//import com.example.messagingtutorialskeleton.R;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.sinch.android.rtc.PushPair;
 import com.sinch.android.rtc.Sinch;
 import com.sinch.android.rtc.SinchClient;
-import com.sinch.android.rtc.messaging.Message;
-import com.sinch.android.rtc.messaging.MessageClient;
-import com.sinch.android.rtc.messaging.MessageClientListener;
-import com.sinch.android.rtc.messaging.MessageDeliveryInfo;
-import com.sinch.android.rtc.messaging.MessageFailureInfo;
+import com.sinch.android.rtc.calling.Call;
+import com.sinch.android.rtc.calling.CallListener;
+
+import java.util.List;
+
+//import com.example.messagingtutorialskeleton.R;
+
 public class ContactsList extends AppCompatActivity {
 
-
+    protected Call call;
+    private class SinchCallListener implements CallListener {
+        @Override
+        public void onCallEnded(Call endedCall) {
+            call = null;
+            call.setText("Call");
+        }
+        @Override
+        public void onCallEstablished(Call establishedCall) {
+            //incoming call was picked up
+        }
+        @Override
+        public void onCallProgressing(Call progressingCall) {
+            //call is ringing
+        }
+        @Override
+        public void onShouldSendPushNotification(Call call, List<PushPair> pushPairs) {
+            //don't worry about this right now
+        }
+    }
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,6 @@ public class ContactsList extends AppCompatActivity {
         setContentView(R.layout.activity_contactslist);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         final SinchClient sinchClient = Sinch.getSinchClientBuilder()
@@ -56,22 +66,69 @@ public class ContactsList extends AppCompatActivity {
         sinchClient.setSupportCalling(true);
         sinchClient.start();
 
-        Button btcall = (Button) findViewById(R.id.btcall);
-        btcall.setOnClickListener(new View.OnClickListener(){
+
+        final Button btcall = (Button) findViewById(R.id.btcall);
+        btcall.setOnClickListener(new View.OnClickListener() {
 
             @Override
-        public void onClick(View vie3w){
+            public void onClick(View vie3w) {
+                if (call == null) {
+                    call = sinchClient.getCallClient().callUser("call-recipient-id");
+                    btcall.setText("Hang Up");
+                } else {
+                    call.hangup();
 
-                sinchClient.getCallClient().callUser("call-recipient-id");
+                }
+                call.addCallListener(new SinchCallListener());
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ContactsList Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.wojtek_asus.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "ContactsList Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.wojtek_asus.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 
 
-
-       // SimpleAdapter adapter = new SimpleAdapter(this, );
-
-
+    // SimpleAdapter adapter = new SimpleAdapter(this, );
 
 
 }
